204e8a9789fcb9402cdbd532dfe5dcd991945e86
lamodadite <102534186+lamodadite@users.noreply.github.com>
2024-01-11 21:25:35 +0900

    feat : Rotate the point of view with the mouse

--- srcs/cu_controll_mouse.c
+++ srcs/cu_controll_mouse.c
@@ -4,14 +4,19 @@ int	controll_mouse(int x, int y, t_info *info)
 {
 	(void) y;
 	int	dir;
+	double tmp_rot_speed;
 
+	tmp_rot_speed = info->rot_speed;
 	dir = 0;
 	if (x - info->pre_mouse_pos_x > 0)
 		dir = K_AR_R;
 	else if (x - info->pre_mouse_pos_x < 0)
 		dir = K_AR_L;
+	if (dir != 0)
+		info->rot_speed /= 2;
 	press_left_rotate_key(dir, info);
 	press_right_rotate_key(dir, info);
 	info->pre_mouse_pos_x = x;
+	info->rot_speed = tmp_rot_speed;
 	return (0);
 }
